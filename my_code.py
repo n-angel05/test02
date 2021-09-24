d0a34ef4b01c7a5dea6327903b72882c266b324f
Nayeli Angel <nayeli.angel.p@gmail.com>
2021-09-24 17:53:03 -0500

    First commit

---
+++ my_code.py
@@ -0,0 +1,22 @@
+def main():
+    n = 6
+    ar = [1, 2, 1, 2, 1, 3, 2]
+    idx = dict()
+
+    for i in ar:
+        if i in idx:
+            idx[i] += 1
+        else: 
+            idx[i] = 1
+
+    print (idx)
+
+    ctr = 0
+    for i in idx:
+        ctr += int(idx[i]/2)
+
+    print (ctr)
+
+if __name__ == "__main__":
+    main()
+
